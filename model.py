a26e3ecced84faaf3a465f141aab538f426df7bf
hthieu <hthieu96@gmail.com>
2018-11-15 01:38:44 -0600

    Can run

--- model.py
+++ model.py
@@ -1,8 +1,10 @@
 from utils import (
-  read_data, 
-  input_setup, 
-  imsave,
-  merge
+    read_data,
+    input_setup,
+    imsave,
+    merge,
+    get_image_path,
+    preprocess
 )
 
 import time
@@ -13,139 +15,171 @@ import numpy as np
 import tensorflow as tf
 
 try:
-  xrange
+    xrange
 except:
-  xrange = range
+    xrange = range
+
 
 class SRCNN(object):
 
-  def __init__(self, 
-               sess, 
-               image_size=33,
-               label_size=21, 
-               batch_size=128,
-               c_dim=1, 
-               checkpoint_dir=None, 
-               sample_dir=None):
+    def __init__(self,
+                 sess,
+                 image_size=28,
+                 label_size=16,
+                 batch_size=128,
+                 c_dim=1,
+                 checkpoint_dir=None,
+                 sample_dir=None):
 
-    self.sess = sess
-    self.is_grayscale = (c_dim == 1)
-    self.image_size = image_size
-    self.label_size = label_size
-    self.batch_size = batch_size
+        self.sess = sess
+        self.is_grayscale = (c_dim == 1)
+        self.image_size = image_size
+        self.label_size = label_size
+        self.batch_size = batch_size
 
-    self.c_dim = c_dim
+        self.c_dim = c_dim
 
-    self.checkpoint_dir = checkpoint_dir
-    self.sample_dir = sample_dir
-    self.build_model()
+        self.checkpoint_dir = checkpoint_dir
+        self.sample_dir = sample_dir
+        self.build_model()
 
-  def build_model(self):
-    self.images = tf.placeholder(tf.float32, [None, self.image_size, self.image_size, self.c_dim], name='images')
-    self.labels = tf.placeholder(tf.float32, [None, self.label_size, self.label_size, self.c_dim], name='labels')
-    
-    self.weights = {
-      'w1': tf.Variable(tf.random_normal([9, 9, 1, 64], stddev=1e-3), name='w1'),
-      'w2': tf.Variable(tf.random_normal([1, 1, 64, 32], stddev=1e-3), name='w2'),
-      'w3': tf.Variable(tf.random_normal([5, 5, 32, 1], stddev=1e-3), name='w3')
-    }
-    self.biases = {
-      'b1': tf.Variable(tf.zeros([64]), name='b1'),
-      'b2': tf.Variable(tf.zeros([32]), name='b2'),
-      'b3': tf.Variable(tf.zeros([1]), name='b3')
-    }
+    def build_model(self):
+        self.images = tf.placeholder(tf.float32, [None, self.image_size, self.image_size, self.c_dim], name='images')
+        self.labels = tf.placeholder(tf.float32, [None, self.label_size, self.label_size, self.c_dim], name='labels')
 
-    self.pred = self.model()
+        self.weights = {
+            'w1': tf.Variable(tf.random_normal([9, 9, 1, 64], stddev=1e-3), name='w1'),
+            'w2': tf.Variable(tf.random_normal([1, 1, 64, 32], stddev=1e-3), name='w2'),
+            'w3': tf.Variable(tf.random_normal([5, 5, 32, 1], stddev=1e-3), name='w3')
+        }
+        self.biases = {
+            'b1': tf.Variable(tf.zeros([64]), name='b1'),
+            'b2': tf.Variable(tf.zeros([32]), name='b2'),
+            'b3': tf.Variable(tf.zeros([1]), name='b3')
+        }
 
-    # Loss function (MSE)
-    self.loss = tf.reduce_mean(tf.square(self.labels - self.pred))
+        self.pred = self.model()
 
-    self.saver = tf.train.Saver()
+        # Loss function (MSE)
+        self.loss = tf.reduce_mean(tf.square(self.labels - self.pred))
 
-  def train(self, config):
-    if config.is_train:
-      input_setup(self.sess, config)
-    else:
-      nx, ny = input_setup(self.sess, config)
+        self.saver = tf.train.Saver()
 
-    if config.is_train:     
-      data_dir = os.path.join('./{}'.format(config.checkpoint_dir), "train.h5")
-    else:
-      data_dir = os.path.join('./{}'.format(config.checkpoint_dir), "test.h5")
+    def train(self, config):
+        if config.is_train:
+            input_setup(self.sess, config)
+        # else:
+        #     nx, ny = input_setup(self.sess, config)
 
-    train_data, train_label = read_data(data_dir)
+        if config.is_train:
+            data_dir = os.path.join('./{}'.format(config.checkpoint_dir), "train.h5")
+        else:
+            data_dir = os.path.join('./{}'.format(config.checkpoint_dir), "test.h5")
 
-    # Stochastic gradient descent with the standard backpropagation
-    self.train_op = tf.train.GradientDescentOptimizer(config.learning_rate).minimize(self.loss)
 
-    tf.initialize_all_variables().run()
-    
-    counter = 0
-    start_time = time.time()
+        # Stochastic gradient descent with the standard backpropagation
+        self.train_op = tf.train.GradientDescentOptimizer(config.learning_rate).minimize(self.loss)
 
-    if self.load(self.checkpoint_dir):
-      print(" [*] Load SUCCESS")
-    else:
-      print(" [!] Load failed...")
+        tf.initialize_all_variables().run()
 
-    if config.is_train:
-      print("Training...")
+        counter = 0
+        start_time = time.time()
 
-      for ep in xrange(config.epoch):
-        # Run by batch images
-        batch_idxs = len(train_data) // config.batch_size
-        for idx in xrange(0, batch_idxs):
-          batch_images = train_data[idx*config.batch_size : (idx+1)*config.batch_size]
-          batch_labels = train_label[idx*config.batch_size : (idx+1)*config.batch_size]
+        if self.load(self.checkpoint_dir):
+            print(" [*] Load SUCCESS")
+        else:
+            print(" [!] Load failed...")
 
-          counter += 1
-          _, err = self.sess.run([self.train_op, self.loss], feed_dict={self.images: batch_images, self.labels: batch_labels})
+        if config.is_train:
+            print("Training...")
+            train_data, train_label = read_data(data_dir)
 
-          if counter % 10 == 0:
-            print("Epoch: [%2d], step: [%2d], time: [%4.4f], loss: [%.8f]" \
-              % ((ep+1), counter, time.time()-start_time, err))
+            for ep in xrange(config.epoch):
+                # Run by batch images
+                batch_idxs = len(train_data) // config.batch_size
+                for idx in xrange(0, batch_idxs):
+                    batch_images = train_data[idx * config.batch_size: (idx + 1) * config.batch_size]
+                    batch_labels = train_label[idx * config.batch_size: (idx + 1) * config.batch_size]
 
-          if counter % 500 == 0:
-            self.save(config.checkpoint_dir, counter)
+                    counter += 1
+                    _, err = self.sess.run([self.train_op, self.loss],
+                                           feed_dict={self.images: batch_images, self.labels: batch_labels})
 
-    else:
-      print("Testing...")
+                    if counter % 10 == 0:
+                        print("Epoch: [%2d], step: [%2d], time: [%4.4f], loss: [%.8f]" \
+                              % ((ep + 1), counter, time.time() - start_time, err))
 
-      result = self.pred.eval({self.images: train_data, self.labels: train_label})
+                    if counter % 500 == 0:
+                    # if counter % 180 == 0:
+                        self.save(config.checkpoint_dir, counter)
 
-      result = merge(result, [nx, ny])
-      result = result.squeeze()
-      image_path = os.path.join(os.getcwd(), config.sample_dir)
-      image_path = os.path.join(image_path, "test_image.png")
-      imsave(result, image_path)
+        else:
+            print("Testing...")
+            train_label = np.zeros((1, 16, 16, 1))
 
-  def model(self):
-    conv1 = tf.nn.relu(tf.nn.conv2d(self.images, self.weights['w1'], strides=[1,1,1,1], padding='VALID') + self.biases['b1'])
-    conv2 = tf.nn.relu(tf.nn.conv2d(conv1, self.weights['w2'], strides=[1,1,1,1], padding='VALID') + self.biases['b2'])
-    conv3 = tf.nn.conv2d(conv2, self.weights['w3'], strides=[1,1,1,1], padding='VALID') + self.biases['b3']
-    return conv3
+            for i in range(1, 5):
+                input_path = get_image_path(config.is_train, 64, i)
+                input_, _ = preprocess(input_path, scale=config.scale)
 
-  def save(self, checkpoint_dir, step):
-    model_name = "SRCNN.model"
-    model_dir = "%s_%s" % ("srcnn", self.label_size)
-    checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
+                h, w = input_.shape
 
-    if not os.path.exists(checkpoint_dir):
-        os.makedirs(checkpoint_dir)
+                # Numbers of sub-images in height and width of image are needed to compute merge operation.
+                nx = ny = 0
+                sub_input_sequence = []
+                for x in range(0, h - config.image_size + 1, config.stride):
+                    nx += 1
+                    ny = 0
+                    for y in range(0, w - config.image_size + 1, config.stride):
+                        ny += 1
+                        sub_input = input_[x:x + config.image_size, y:y + config.image_size]  # [33 x 33]
+                        # sub_label = label_[x + int(padding):x + int(padding) + config.label_size,
+                        #             y + int(padding):y + int(padding) + config.label_size]  # [21 x 21]
 
-    self.saver.save(self.sess,
-                    os.path.join(checkpoint_dir, model_name),
-                    global_step=step)
+                        sub_input = sub_input.reshape([config.image_size, config.image_size, 1])
+                        # sub_label = sub_label.reshape([config.label_size, config.label_size, 1])
 
-  def load(self, checkpoint_dir):
-    print(" [*] Reading checkpoints...")
-    model_dir = "%s_%s" % ("srcnn", self.label_size)
-    checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
+                        sub_input_sequence.append(sub_input)
 
-    ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
-    if ckpt and ckpt.model_checkpoint_path:
-        ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
-        self.saver.restore(self.sess, os.path.join(checkpoint_dir, ckpt_name))
-        return True
-    else:
-        return False
+                train_data = np.asarray(sub_input_sequence)
+
+                result = self.pred.eval({self.images: train_data, self.labels: train_label})
+
+                result = merge(result, [nx, ny])
+                result = result.squeeze()
+                image_path = os.path.join(os.getcwd(), config.sample_dir)
+                image_path = os.path.join(image_path, "test_image_"+format(i, "05")+".png")
+                imsave(result, image_path)
+
+    def model(self):
+        conv1 = tf.nn.relu(
+            tf.nn.conv2d(self.images, self.weights['w1'], strides=[1, 1, 1, 1], padding='VALID') + self.biases['b1'])
+        conv2 = tf.nn.relu(
+            tf.nn.conv2d(conv1, self.weights['w2'], strides=[1, 1, 1, 1], padding='VALID') + self.biases['b2'])
+        conv3 = tf.nn.conv2d(conv2, self.weights['w3'], strides=[1, 1, 1, 1], padding='VALID') + self.biases['b3']
+        return conv3
+
+    def save(self, checkpoint_dir, step):
+        model_name = "SRCNN.model"
+        model_dir = "%s_%s" % ("srcnn", self.label_size)
+        checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
+        print(" [*] Saving checkpoints to " + checkpoint_dir)
+
+        if not os.path.exists(checkpoint_dir):
+            os.makedirs(checkpoint_dir)
+
+        self.saver.save(self.sess,
+                        os.path.join(checkpoint_dir, model_name),
+                        global_step=step)
+
+    def load(self, checkpoint_dir):
+        print(" [*] Reading checkpoints...")
+        model_dir = "%s_%s" % ("srcnn", self.label_size)
+        checkpoint_dir = os.path.join(checkpoint_dir, model_dir)
+
+        ckpt = tf.train.get_checkpoint_state(checkpoint_dir)
+        if ckpt and ckpt.model_checkpoint_path:
+            ckpt_name = os.path.basename(ckpt.model_checkpoint_path)
+            self.saver.restore(self.sess, os.path.join(checkpoint_dir, ckpt_name))
+            return True
+        else:
+            return False
--- utils.py
+++ utils.py
@@ -12,195 +12,232 @@ from PIL import Image  # for loading images as YCbCr format
 import scipy.misc
 import scipy.ndimage
 import numpy as np
+import cv2
 
 import tensorflow as tf
 
 try:
-  xrange
+    xrange
 except:
-  xrange = range
-  
+    xrange = range
+
 FLAGS = tf.app.flags.FLAGS
 
+
 def read_data(path):
-  """
-  Read h5 format data file
-  
-  Args:
-    path: file path of desired file
-    data: '.h5' file format that contains train data values
-    label: '.h5' file format that contains train label values
-  """
-  with h5py.File(path, 'r') as hf:
-    data = np.array(hf.get('data'))
-    label = np.array(hf.get('label'))
-    return data, label
+    """
+    Read h5 format data file
 
-def preprocess(path, scale=3):
-  """
-  Preprocess single image file 
-    (1) Read original image as YCbCr format (and grayscale as default)
-    (2) Normalize
-    (3) Apply image file with bicubic interpolation
+    Args:
+      path: file path of desired file
+      data: '.h5' file format that contains train data values
+      label: '.h5' file format that contains train label values
+    """
+    with h5py.File(path, 'r') as hf:
+        data = np.array(hf.get('data'))
+        label = np.array(hf.get('label'))
+        return data, label
 
-  Args:
-    path: file path of desired file
-    input_: image applied bicubic interpolation (low-resolution)
-    label_: image with original resolution (high-resolution)
-  """
-  image = imread(path, is_grayscale=True)
-  label_ = modcrop(image, scale)
 
-  # Must be normalized
-  image = image / 255.
-  label_ = label_ / 255.
+def preprocess(image_path, label_path="", scale=2):
+    """
+    Preprocess single image file
+      (1) Read original image as YCbCr format (and grayscale as default)
+      (2) Normalize
+      (3) Apply image file with bicubic interpolation
 
-  input_ = scipy.ndimage.interpolation.zoom(label_, (1./scale), prefilter=False)
-  input_ = scipy.ndimage.interpolation.zoom(input_, (scale/1.), prefilter=False)
+    Args:
+      path: file path of desired file
+      input_: image applied bicubic interpolation (low-resolution)
+      label_: image with original resolution (high-resolution)
+    """
+    label = None
+    image = imread(image_path)
+    image = image / 255.
 
-  return input_, label_
+    if label_path != "":
+        label = imread(label_path)
+        label = label / 255.
 
-def prepare_data(sess, dataset):
-  """
-  Args:
-    dataset: choose train dataset or test dataset
-    
-    For train dataset, output data would be ['.../t1.bmp', '.../t2.bmp', ..., '.../t99.bmp']
-  """
-  if FLAGS.is_train:
-    filenames = os.listdir(dataset)
-    data_dir = os.path.join(os.getcwd(), dataset)
-    data = glob.glob(os.path.join(data_dir, "*.bmp"))
-  else:
-    data_dir = os.path.join(os.sep, (os.path.join(os.getcwd(), dataset)), "Set5")
-    data = glob.glob(os.path.join(data_dir, "*.bmp"))
+    input_ = scipy.ndimage.interpolation.zoom(image, (scale / 1.), prefilter=False)
 
-  return data
+    return input_, label
+
+
+def get_image_path(is_train, s, num):
+    assert (s == 128 or s == 64)
+    path = os.path.join(os.getcwd(), "xray_images/")
+    image_name = ""
+    if not is_train:
+        path += 'test_images_'
+        image_name += 'test_'
+    else:
+        path += 'train_images_'
+        image_name += 'train_'
+    if s == 64:
+        path += '64x64'
+    elif s == 128:
+        path += '128x128'
+    num_str = format(num, "05")
+    image_name += num_str + ".png"
+    return path + "/" + image_name
+
+
+# def prepare_data():
+#     """
+#     Args:
+#       dataset: choose train dataset or test dataset
+#
+#       For train dataset, output data would be ['.../t1.bmp', '.../t2.bmp', ..., '.../t99.bmp']
+#     """
+#     inputs_paths = None
+#     labels_paths = None
+#     if FLAGS.is_train:
+#         inputs_dir = os.path.join(os.getcwd(), "xray_images/test_images_64x64/")
+#         labels_dir = os.path.join(os.getcwd(), "xray_images/test_images_128x128/")
+#         for i in range(4000, 20000):
+#             input_path = inputs_dir + "train_" + format(i, "05") + ".png"
+#             label_path = labels_dir + "train_" + format(i, "05") + ".png"
+#
+#         filenames = os.listdir(dataset)
+#         data_dir = os.path.join(os.getcwd(), dataset)
+#         data_path = glob.glob(os.path.join(data_dir, "*.png"))
+#     else:
+#         data_dir = os.path.join(os.sep, (os.path.join(os.getcwd(), dataset)), "Set5")
+#         data_path = glob.glob(os.path.join(data_dir, "*.png"))
+#
+#     return data_path
+
 
 def make_data(sess, data, label):
-  """
-  Make input data as h5 file format
-  Depending on 'is_train' (flag value), savepath would be changed.
-  """
-  if FLAGS.is_train:
-    savepath = os.path.join(os.getcwd(), 'checkpoint/train.h5')
-  else:
-    savepath = os.path.join(os.getcwd(), 'checkpoint/test.h5')
+    """
+    Make input data as h5 file format
+    Depending on 'is_train' (flag value), savepath would be changed.
+    """
+    if FLAGS.is_train:
+        savepath = os.path.join(os.getcwd(), 'checkpoint/train.h5')
+    else:
+        savepath = os.path.join(os.getcwd(), 'checkpoint/test.h5')
 
-  with h5py.File(savepath, 'w') as hf:
-    hf.create_dataset('data', data=data)
-    hf.create_dataset('label', data=label)
+    with h5py.File(savepath, 'w') as hf:
+        hf.create_dataset('data', data=data)
+        hf.create_dataset('label', data=label)
 
-def imread(path, is_grayscale=True):
-  """
-  Read image using its path.
-  Default value is gray-scale, and image is read by YCbCr format as the paper said.
-  """
-  if is_grayscale:
-    return scipy.misc.imread(path, flatten=True, mode='YCbCr').astype(np.float)
-  else:
-    return scipy.misc.imread(path, mode='YCbCr').astype(np.float)
 
-def modcrop(image, scale=3):
-  """
-  To scale down and up the original image, first thing to do is to have no remainder while scaling operation.
-  
-  We need to find modulo of height (and width) and scale factor.
-  Then, subtract the modulo from height (and width) of original image size.
-  There would be no remainder even after scaling operation.
-  """
-  if len(image.shape) == 3:
-    h, w, _ = image.shape
-    h = h - np.mod(h, scale)
-    w = w - np.mod(w, scale)
-    image = image[0:h, 0:w, :]
-  else:
-    h, w = image.shape
-    h = h - np.mod(h, scale)
-    w = w - np.mod(w, scale)
-    image = image[0:h, 0:w]
-  return image
+def imread(path):
+    """
+    Read image using its path.
+    Default value is gray-scale, and image is read by YCbCr format as the paper said.
+    """
+    return cv2.imread(path, cv2.IMREAD_GRAYSCALE).astype(float)
+
+
+# def modcrop(image, scale=3):
+#     """
+#     To scale down and up the original image, first thing to do is to have no remainder while scaling operation.
+#
+#     We need to find modulo of height (and width) and scale factor.
+#     Then, subtract the modulo from height (and width) of original image size.
+#     There would be no remainder even after scaling operation.
+#     """
+#     if len(image.shape) == 3:
+#         h, w, _ = image.shape
+#         h = h - np.mod(h, scale)
+#         w = w - np.mod(w, scale)
+#         image = image[0:h, 0:w, :]
+#     else:
+#         h, w = image.shape
+#         h = h - np.mod(h, scale)
+#         w = w - np.mod(w, scale)
+#         image = image[0:h, 0:w]
+#     return image
+
 
 def input_setup(sess, config):
-  """
-  Read image files and make their sub-images and saved them as a h5 file format.
-  """
-  # Load data path
-  if config.is_train:
-    data = prepare_data(sess, dataset="Train")
-  else:
-    data = prepare_data(sess, dataset="Test")
+    """
+    Read image files and make their sub-images and saved them as a h5 file format.
+    """
+    # Load data path
+    # if config.is_train:
+    #   data = prepare_data(sess, True)
+    # else:
+    #   data = prepare_data(sess, dataset="Test")
+    # data = prepare_data(sess, config.is_train)
 
-  sub_input_sequence = []
-  sub_label_sequence = []
-  padding = abs(config.image_size - config.label_size) / 2 # 6
+    sub_input_sequence = []
+    sub_label_sequence = []
+    padding = abs(config.image_size - config.label_size) / 2  # 6
 
-  if config.is_train:
-    for i in xrange(len(data)):
-      input_, label_ = preprocess(data[i], config.scale)
+    if config.is_train:
+        for i in xrange(4000, 20000):
+            input_path = get_image_path(config.is_train, 64, i)
+            label_path = get_image_path(config.is_train, 128, i)
+            input_, label_ = preprocess(input_path, label_path, config.scale)
 
-      if len(input_.shape) == 3:
-        h, w, _ = input_.shape
-      else:
-        h, w = input_.shape
+            h, w = input_.shape
 
-      for x in range(0, h-config.image_size+1, config.stride):
-        for y in range(0, w-config.image_size+1, config.stride):
-          sub_input = input_[x:x+config.image_size, y:y+config.image_size] # [33 x 33]
-          sub_label = label_[x+int(padding):x+int(padding)+config.label_size, y+int(padding):y+int(padding)+config.label_size] # [21 x 21]
+            for x in range(0, h - config.image_size + 1, config.stride):
+                for y in range(0, w - config.image_size + 1, config.stride):
+                    sub_input = input_[x:x + config.image_size, y:y + config.image_size]  # [33 x 33]
+                    sub_label = label_[x + int(padding):x + int(padding) + config.label_size,
+                                y + int(padding):y + int(padding) + config.label_size]  # [21 x 21]
 
-          # Make channel value
-          sub_input = sub_input.reshape([config.image_size, config.image_size, 1])  
-          sub_label = sub_label.reshape([config.label_size, config.label_size, 1])
+                    # Make channel value
+                    sub_input = sub_input.reshape([config.image_size, config.image_size, 1])
+                    sub_label = sub_label.reshape([config.label_size, config.label_size, 1])
 
-          sub_input_sequence.append(sub_input)
-          sub_label_sequence.append(sub_label)
+                    sub_input_sequence.append(sub_input)
+                    sub_label_sequence.append(sub_label)
 
-  else:
-    input_, label_ = preprocess(data[2], config.scale)
-
-    if len(input_.shape) == 3:
-      h, w, _ = input_.shape
     else:
-      h, w = input_.shape
+        for i in range(1, 2):
+            input_path = get_image_path(config.is_train, 64, i)
+            input_, _ = preprocess(input_path, scale=config.scale)
 
-    # Numbers of sub-images in height and width of image are needed to compute merge operation.
-    nx = ny = 0 
-    for x in range(0, h-config.image_size+1, config.stride):
-      nx += 1; ny = 0
-      for y in range(0, w-config.image_size+1, config.stride):
-        ny += 1
-        sub_input = input_[x:x+config.image_size, y:y+config.image_size] # [33 x 33]
-        sub_label = label_[x+int(padding):x+int(padding)+config.label_size, y+int(padding):y+int(padding)+config.label_size] # [21 x 21]
-        
-        sub_input = sub_input.reshape([config.image_size, config.image_size, 1])  
-        sub_label = sub_label.reshape([config.label_size, config.label_size, 1])
+            h, w = input_.shape
 
-        sub_input_sequence.append(sub_input)
-        sub_label_sequence.append(sub_label)
+            # Numbers of sub-images in height and width of image are needed to compute merge operation.
+            nx = ny = 0
+            for x in range(0, h - config.image_size + 1, config.stride):
+                nx += 1
+                ny = 0
+                for y in range(0, w - config.image_size + 1, config.stride):
+                    ny += 1
+                    sub_input = input_[x:x + config.image_size, y:y + config.image_size]  # [33 x 33]
+                    # sub_label = label_[x + int(padding):x + int(padding) + config.label_size,
+                    #             y + int(padding):y + int(padding) + config.label_size]  # [21 x 21]
 
-  """
-  len(sub_input_sequence) : the number of sub_input (33 x 33 x ch) in one image
-  (sub_input_sequence[0]).shape : (33, 33, 1)
-  """
-  # Make list to numpy array. With this transform
-  arrdata = np.asarray(sub_input_sequence) # [?, 33, 33, 1]
-  arrlabel = np.asarray(sub_label_sequence) # [?, 21, 21, 1]
+                    sub_input = sub_input.reshape([config.image_size, config.image_size, 1])
+                    # sub_label = sub_label.reshape([config.label_size, config.label_size, 1])
 
-  make_data(sess, arrdata, arrlabel)
+                    sub_input_sequence.append(sub_input)
+                    # sub_label_sequence.append(sub_label)
 
-  if not config.is_train:
-    return nx, ny
-    
+    """
+    len(sub_input_sequence) : the number of sub_input (33 x 33 x ch) in one image
+    (sub_input_sequence[0]).shape : (33, 33, 1)
+    """
+    # Make list to numpy array. With this transform
+    arrdata = np.asarray(sub_input_sequence)  # [?, 33, 33, 1]
+    arrlabel = np.asarray(sub_label_sequence)  # [?, 21, 21, 1]
+
+    if config.is_train:
+        make_data(sess, arrdata, arrlabel)
+    if not config.is_train:
+        arrlabel = np.zeros((1, 16, 16, 1))
+        make_data(sess, arrdata, arrlabel)
+        return nx, ny
+
+
 def imsave(image, path):
-  return scipy.misc.imsave(path, image)
+    return scipy.misc.imsave(path, image)
+
 
 def merge(images, size):
-  h, w = images.shape[1], images.shape[2]
-  img = np.zeros((h*size[0], w*size[1], 1))
-  for idx, image in enumerate(images):
-    i = idx % size[1]
-    j = idx // size[1]
-    img[j*h:j*h+h, i*w:i*w+w, :] = image
-
-  return img
+    h, w = images.shape[1], images.shape[2]
+    img = np.zeros((h * size[0], w * size[1], 1))
+    for idx, image in enumerate(images):
+        i = idx % size[1]
+        j = idx // size[1]
+        img[j * h:j * h + h, i * w:i * w + w, :] = image
+    return img
